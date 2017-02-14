88f94f7cab6c13b4d9ab16332a5555e59253e48b
Ivan Klus <ivanklus@live.com>
2017-02-14 18:00:50 -0500

    Added the start of new VR website redesign
    I want to shocase my work in a VR Treehouse.

---
+++ js/use-trapdoor.js
@@ -0,0 +1,23 @@
+// Opens and closes the Trapdoor
+
+AFRAME.registerComponent('use-trapdoor', {
+  schema: {
+    on: {type: 'string'},
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+    var openFlag = false;
+
+    el.addEventListener(data.on, function () {
+      if (openFlag) {
+         el.emit('closeTrapdoor');
+          openFlag = false;
+      } else {
+          el.emit('openTrapdoor');
+          openFlag = true;
+      }
+    });
+  }
+});
